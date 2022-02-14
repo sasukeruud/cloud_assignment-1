e043a7ac919fc9220d6d1bbcef980dfe0074fb6f
Robin <robin1998@hotmail.no>
2022-02-14 13:49:36 +0100

    added structs for Uni and Country

--- app/struct/struct.go
+++ app/struct/struct.go
@@ -1,4 +1,21 @@
 package structs
 
+//"json"
+
 type Page struct {
+}
+
+type Uni struct {
+	webpage        string `json:"web_pages"`
+	name           string `json:"name"`
+	aplha_two_code string `json:"aplha_two_code`
+	state_province string `json:"state-province"`
+	domains        string `json:"domains"`
+	country        string `json:"country"`
+}
+
+type Country struct {
+	name    string `json:"name"`
+	region  string `json:"region"`
+	capital string `json:"captial"`
 }
