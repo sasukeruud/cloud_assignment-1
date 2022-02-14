a65b980d88f2faa8b40f4bc031fb8ed508b1e4a3
Robin <robin1998@hotmail.no>
2022-02-14 15:47:07 +0100

    localhost:8080 with hello writen

--- app/main.go
+++ app/main.go
@@ -2,37 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
-	//hipo "https://github.com/Hipo/university-domains-list.git"
+	//"./structs"
+	//"strings"
+	//"log"
 )
-
-/*
-* Struct for a page, this is how it will be saved in memory
-* Code from: https://go.dev/doc/articles/wiki/	*/
-type Page struct {
-	Title string
-	Body  []byte
-}
 
 /*
 * Function to save the body of the website in a .txt file named after the "Title"
 * p --> Pointer to a object of a page
 * return --> it will return an error (return value "0600" is for read write permissons for current user)
-* Code from: https://go.dev/doc/articles/wiki/	*/
-func (p *Page) save() error {
+* Code from: https://go.dev/doc/articles/wiki/
+func (p *structs.Page) savePage() error {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
-}
-
-/*
-* Function to get a spesific port to run the application on.
-* It's needed for the application to run on heroku where to application is run on.*/
-func getport() string {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	return port
 }
 
 /*
@@ -44,16 +28,35 @@ func getport() string {
 * return --> If there is an error while the function is trying to load the
 *			file it will return an error, else it will return the file with
 *			the parameters for a webpage.
-* Code from: https://go.dev/doc/articles/wiki/	*/
-func loadPage(title string) (*Page, error) {
+* Code from: https://go.dev/doc/articles/wiki/
+func loadPage(title string) (*structs.Page, error) {
 	filename := title + ".txt"
 	body, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: title, Body: body}, nil
+	return &structs.Page{Title: title, Body: body}, nil
+}
+
+*/
+
+/*
+* Function to get a spesific port to run the application on.
+* It's needed for the application to run on heroku where to application is run on.*/
+func getport() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8080"
+	}
+	return port
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Helllo, %s!", r.URL.Path[1:])
 }
 
 func main() {
 	fmt.Println("hello world")
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(getport(), nil)
 }
