486c14d1803c226b72840c2778567d76ae738263
Robin <robin1998@hotmail.no>
2022-02-16 11:17:37 +0100

    Added search function to handlerCountry

--- app/handlers/handlerCountry.go
+++ app/handlers/handlerCountry.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	read "assignment_1/app/readData"
 	"assignment_1/app/structs"
 	"encoding/json"
 	"net/http"
+	"path"
 )
 
 /*
@@ -23,11 +25,10 @@ Handler for REST get for Country
 code taken from: https://git.gvk.idi.ntnu.no/course/prog2005/prog2005-2022/-/tree/main/02-JSON-demo
 It have been modified to fit this application*/
 func handlerGetRequestCountry(w http.ResponseWriter, r *http.Request) {
-	country := structs.Country{
-		Name:           "Norge",
-		Aplha_two_code: "NO",
-		Region:         "Europe",
-		Capital:        "Oslo"}
+	search := path.Base(r.URL.Path)
+
+	var country []structs.Country
+	json.Unmarshal(read.ReadCountriesAPI(search), country)
 
 	w.Header().Add("contet-type", "application/json")
 
--- app/readData/readJson.go
+++ app/readData/readJson.go
@@ -35,7 +35,7 @@ Function to read information from the https://restcountries.com/v3.1/ api.
 The const READ_ALL_COUNTRIES is the URL for the api.
 var search string -> varible for what to be search for
 return responseData -> returns a byte slice of information */
-func readCountriesAPI(search string) []byte {
+func ReadCountriesAPI(search string) []byte {
 	response, err := http.Get(constants.READ_ALL_COUNTRIES + search)
 
 	if err != nil {
