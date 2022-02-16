fc4a30f34e454465c0ae5baa65ba2f8855306c65
Robin <robin1998@hotmail.no>
2022-02-16 10:47:42 +0100

    removed POST method from the handlerUni

--- app/handlers/handlerUni.go
+++ app/handlers/handlerUni.go
@@ -4,7 +4,6 @@ import (
 	read "assignment_1/app/readData"
 	"assignment_1/app/structs"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"path"
 )
@@ -14,38 +13,11 @@ import (
  * code taken from: https://git.gvk.idi.ntnu.no/course/prog2005/prog2005-2022/-/tree/main/02-JSON-demo */
 func UniHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	case http.MethodPost:
-		handlerPostRequestUni(w, r)
 	case http.MethodGet:
 		handlerGetRequestUni(w, r)
 	default:
 		http.Error(w, "Method not supported. Currentlyonly GET and 	POST are supported", http.StatusNotImplemented)
 	}
-}
-
-/*
-* Handler for REST post for Uni
-* code taken from: https://git.gvk.idi.ntnu.no/course/prog2005/prog2005-2022/-/tree/main/02-JSON-demo
-* It have been modified to fit this application*/
-func handlerPostRequestUni(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	uni := structs.Uni{}
-
-	err := decoder.Decode(&uni)
-	if err != nil {
-		http.Error(w, "Error during decoding", http.StatusInternalServerError)
-		return
-	}
-
-	output, err := json.MarshalIndent(uni, "", "")
-	if err != nil {
-		http.Error(w, "Error during pretty printing", http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Println(string(output))
-
-	http.Error(w, "OK", http.StatusOK)
 }
 
 /*
