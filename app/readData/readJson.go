6f619e750a90260c7ce0fc95ccdaf8b78708e872
Robin <robin1998@hotmail.no>
2022-02-17 16:00:01 +0100

    rewritten comments

--- app/readData/readJson.go
+++ app/readData/readJson.go
@@ -12,16 +12,14 @@ import (
 	"strings"
 )
 
-var uniInfo []structs.UniInfo
-
-//var CountryInfo structs.Country
-
 /*
 Function to read information from the http://universities.hipolabs.com/ api.
 the const READ_ALL_UNI is the URL for the api.
 var search string -> varible for what to be search for.
-return responseData -> returns a byte slice of information */
+return uniInfo -> returns slice information about the universety */
 func ReadUniAPI(search string) []structs.UniInfo {
+	var uniInfo []structs.UniInfo
+
 	response, err := http.Get(constants.READ_ALL_UNI + search)
 
 	if err != nil {
@@ -46,10 +44,10 @@ func ReadUniAPI(search string) []structs.UniInfo {
 Function to read information from the https://restcountries.com/v3.1/ api.
 The const READ_ALL_COUNTRIES is the URL for the api.
 var search string -> varible for what to be search for
-return responseData -> returns a byte slice of information */
+return countryInfo -> returns a single country information in a slice. Returns in a slice because json saves information in slices*/
 func ReadCountriesAPI(search string) []structs.Country {
 
-	var CountryInfo []structs.Country
+	var countryInfo []structs.Country
 	response, err := http.Get(constants.READ_ALL_COUNTRIES + search)
 
 	if err != nil {
@@ -62,9 +60,7 @@ func ReadCountriesAPI(search string) []structs.Country {
 		log.Fatal(err)
 	}
 
-	json.Unmarshal(responseData, &CountryInfo)
+	json.Unmarshal(responseData, &countryInfo)
 
-	fmt.Println(CountryInfo[0].Name.Common)
-
-	return CountryInfo
+	return countryInfo
 }
