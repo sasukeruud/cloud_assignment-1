2af4a6862da131200772031bf371a0234aa817b5
Robin <robin1998@hotmail.no>
2022-02-18 13:29:38 +0100

    Added skeleton for handler

--- app/handlers/handlerNeighbourUnis.go
+++ app/handlers/handlerNeighbourUnis.go
@@ -1,1 +1,37 @@
 package handlers
+
+import (
+	read "assignment_1/app/readData"
+	"encoding/json"
+	"net/http"
+	"path"
+)
+
+/*
+Function to handle what type of request for the application to handle from uni
+code taken from: https://git.gvk.idi.ntnu.no/course/prog2005/prog2005-2022/-/tree/main/02-JSON-demo */
+func UniNeighborHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		handlerGetRequestUniNeighbor(w, r)
+	default:
+		http.Error(w, "Method not supported. Currentlyonly GET are supported", http.StatusNotImplemented)
+	}
+}
+
+/*
+Handler for REST get for Uni
+some of the code is taken from: https://git.gvk.idi.ntnu.no/course/prog2005/prog2005-2022/-/tree/main/02-JSON-demo
+It have been modified to fit this application*/
+func handlerGetRequestUniNeighbor(w http.ResponseWriter, r *http.Request) {
+	search := path.Base(r.URL.Path)
+
+	w.Header().Set("contet-type", "application/json")
+
+	encoder := json.NewEncoder(w)
+
+	err := encoder.Encode(read.ReadUniInfo(search))
+	if err != nil {
+		http.Error(w, "Error during encoding", http.StatusInternalServerError)
+	}
+}
