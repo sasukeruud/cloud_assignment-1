6a97979d2f106ee36838a090cb36faaa7cfc6844
Robin <robin1998@hotmail.no>
2022-02-24 21:35:50 +0100

    completed default handler

--- app/handlers/handlerDefault.go
+++ app/handlers/handlerDefault.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+/*
+Function to handle access to the deafult API page.
+code taken from: https://git.gvk.idi.ntnu.no/course/prog2005/prog2005-2022/-/tree/main/02-JSON-demo
+It has been modified to fit this application  */
 func DeafultHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	default:
@@ -12,6 +16,11 @@ func DeafultHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+/*
+Default page for when the API*/
 func defaultPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "To use the api use the url with either of these:\n")
+	fmt.Fprintf(w, "/unisearcher/v1/uniinfo/ \n")
+	fmt.Fprintf(w, "/unisearcher/v1/neighbourunis/\n")
+	fmt.Fprintf(w, "/unisearcher/v1/diag/\n")
 }
